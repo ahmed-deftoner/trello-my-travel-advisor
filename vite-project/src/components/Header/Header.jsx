7063fc4697859fb1d4f3ee37fa394cd6568c8c8d
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-11 12:35:25 +0500

    header

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,11 +1,35 @@
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Header = () => {
+import useStyles from './styles.js';
+import { green } from '@material-ui/core/colors';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
 
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+         {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>*/}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          {/*</Autocomplete>*/}
+          </Box>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
-export default Header
+export default Header;
