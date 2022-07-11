96c756632cd26c700e40c0b40f5f5c4e782147ad
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-11 12:18:27 +0500

    initial layout

---
+++ vite-project/src/components/List/List.jsx
@@ -0,0 +1,11 @@
+
+const List = () => {
+
+  return (
+    <div>
+      <h1>list</h1>
+    </div>
+  );
+}
+
+export default List
---
+++ vite-project/src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+
+const PlaceDetails = () => {
+
+  return (
+    <div>
+      <h1>details</h1>
+    </div>
+  );
+}
+
+export default PlaceDetails
